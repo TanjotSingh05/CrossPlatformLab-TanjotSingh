1cbd1fad4033e58adc9840f5e3e8afe20d148134
tanjotsingh <tanjotsingh@localhost>
2026-02-19 17:46:43 -0500

    Initial commit

---
+++ src/FileManager/FileManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FileManager;
+
+/**
+ *
+ * @author tanjotsingh
+ */
+public class FileManager {
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+    
+}
